6d310b14972b3a6b567794dcaefc99936c95a314
krassot1n <krassotin68@gmail.com>
2023-02-25 04:49:46 +0800

    Проект создан

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+
+    }
+
+    private static void task1() {
+        System.out.println("Задача №1,2,3");
+        String firstName = "Ivanov ";
+        String midletName = " Ivan";
+        String lastName = " Ivanovich";
+        String fullName = firstName + midletName + lastName;
+        System.out.println("ФИО сотрудника - " + fullName);
+    }
+
+    public static void task2() {
+        System.out.println("Задача №2");
+        String firstName = "Ivanov ";
+        String midletName = " Ivan";
+        String lastName = " Ivanovich";
+        String fullName = firstName + midletName + lastName;
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " +  fullName.toUpperCase());
+    }
+    private static void task3() {
+        System.out.println("Задача №3");
+        String fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё","е");
+        System.out.println("Данные ФИО сотрудника —" + fullName);
+    }
+}
